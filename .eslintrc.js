cc422987f7956baf3b2ac13d19992f0109f91154
Vinícius Ferreira <vinidotnet@gmail.com>
2021-09-21 02:31:53 -0300

    init: boilerplate configured

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb-base",
+    "plugin:prettier/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier", "import"],
+  rules: {
+    "prettier/prettier": "error",
+    "import/extensions": "off",
+    "no-console": "off",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "./tsconfig.json",
+      },
+    },
+  },
+}
---
+++ src/math/add.ts
@@ -0,0 +1,9 @@
+const add = (a: number, b: number): number => {
+  return a + b
+}
+
+export function otherAdd(): string {
+  return "other one"
+}
+
+export default add
